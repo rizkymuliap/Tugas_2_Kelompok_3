b27be42803e8d13022f474250773bdd532888bb4
rizkymuliap <perdanarizkymulia162@gmail.com>
2023-09-29 13:57:32 +0700

    first update

---
+++ Tugas_2_Kelompok_3/Controllers/Jenis_PeralatanController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tugas_2_Kelompok_3.Models;
+
+namespace Tugas_2_Kelompok_3.Controllers
+{
+    public class Jenis_PeralatanController : Controller
+    {
+        private static List<Jenis_Peralatan> jenis_Peralatans = InitializeData();
+
+        private static List<Jenis_Peralatan> InitializeData()
+        {
+            List<Jenis_Peralatan> initialData = new List<Jenis_Peralatan>
+            {
+                new Jenis_Peralatan
+                {
+                    Id = 1,
+                    nama_jenis_peralatan = "Boyolali",
+                    deskripsi_jenis_peralatan = "Roni Prasetyo",
+                    status = 1
+                },
+                new Jenis_Peralatan
+                {
+                    Id = 2,
+                    nama_jenis_peralatan = "Boyolali",
+                    deskripsi_jenis_peralatan = "Roni Prasetyo",
+                    status = 1
+                }
+            };
+            return initialData;
+        }
+        public IActionResult Index()
+        {
+            List<Jenis_Peralatan> jenis_Peralatanlist = jenis_Peralatans.ToList();
+            return View(jenis_Peralatanlist);
+        }
+
+
+    }
+}
---
+++ Tugas_2_Kelompok_3/Models/Jenis_Peralatan.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tugas_2_Kelompok_3.Models
+{
+    public class Jenis_Peralatan
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nama jenis peralaltan wajib diisi.")]
+        [MaxLength(30, ErrorMessage = "Nama jenis peralatan maksimal 30 karakter.")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Nama jenis peralatan hanya boleh berisi huruf.")]
+        public string nama_jenis_peralatan { get; set; }
+
+        [Required(ErrorMessage = "Deskripsi jenis peralatan wajib diisi.")]
+        [MaxLength(50, ErrorMessage = "Deskripsi jenis peralatan maksimal 50 karakter.")]
+        public string deskripsi_jenis_peralatan { get; set; }
+
+        [Range(0, 1, ErrorMessage = "Status tidak valid.")]
+        public int status { get; set; }
+    }
+}
