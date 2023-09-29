1d319e92d138a5938af4310bd07ef0ab1d594e80
rifkimaulana <rifkim0811@gmail.com>
2023-09-29 16:09:02 +0700

    model

---
+++ Tugas_2_Kelompok_3/Models/Peralatan.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tugas_2_Kelompok_3.Models
+{
+    public class Peralatan
+    {
+        public int Id { get; set; }
+
+        public int Id_jenis {  get; set; }
+
+        [Required(ErrorMessage = "Nama peralatan wajib diisi.")]
+        [MaxLength(30, ErrorMessage = "Nama peralatan maksimal 30 karakter.")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Nama peralatan hanya boleh berisi huruf.")]
+        public string nama_peralatan { get; set; }
+
+        [Required(ErrorMessage = "Deskripsi peralatan wajib diisi.")]
+        [MaxLength(50, ErrorMessage = "Deskripsi peralatan maksimal 50 karakter.")]
+        public string deskripsi_peralatan { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Harga hanya boleh diisi dengan angka.")]
+        public int Harga_Beli {  get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Stok hanya boleh diisi dengan angka.")]
+        public int stok { get; set; }
+
+        [Range(0, 1, ErrorMessage = "Status tidak valid.")]
+        public int status { get; set; }
+    }
+}
---
+++ Tugas_2_Kelompok_3/Models/Transaksi.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Tugas_2_Kelompok_3.Models
+{
+    using System;
+
+    public class Transaksi
+    {
+        
+        public int Id { get; set; }
+
+        public int ID_Peralatan { get; set; }
+
+        public DateTime Tanggal_Transaksi { get; set; }
+
+        public string Jenis_Transaksi { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Jumlah Item hanya boleh diisi dengan angka.")]
+        public int Jumlah_Item { get; set; }
+   
+        public int Total_Harga { get; set; }
+
+        [Required(ErrorMessage = "Nama Pelanggan wajib diisi.")]
+        [MaxLength(30, ErrorMessage = "Nama Pelanggan maksimal 30 karakter.")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Nama Pelanggan hanya boleh berisi huruf.")]
+        public string Nama_Pelanggan { get; set; }
+
+        [Required(ErrorMessage = "Metode Pembayaran wajib diisi.")]
+        public string Metode_Pembayaran { get; set; }
+
+        [Required(ErrorMessage = "Status Pengiriman wajib diisi.")]
+        public string Status_Pengiriman { get; set; }
+
+        [Required(ErrorMessage = "Alamat Pengiriman wajib diisi.")]
+        public string Alamat_Pengiriman { get; set; }
+    }
+}
