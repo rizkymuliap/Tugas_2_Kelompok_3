ac052c2aa81217876c30743f507425c32b42392b
anisatulfatikha <anisatulfatikha2004@gmail.com>
2023-10-02 08:37:39 +0700

    edit home controller

--- Tugas_2_Kelompok_3/Controllers/HomeController.cs
+++ Tugas_2_Kelompok_3/Controllers/HomeController.cs
@@ -7,9 +7,6 @@ namespace Tugas_2_Kelompok_3.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private static List<Peralatan> peralatans = InitializeData();
-
-
 
         public HomeController(ILogger<HomeController> logger)
         {
